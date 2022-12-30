74a32a11518ec5c66a12cb90fcedc1a87329923a
[CInHyeok] <[dlsgur6965@naver.com]>
2022-12-30 20:58:50 +0900

    커밋 메세지

---
+++ 201715563  othello/CS/eCoinColor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _201715563__othello.CS
+{
+    public enum eCoinColor
+    {
+        Black,
+        White,
+        Empty,
+    }
+}
---
+++ 201715563  othello/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _201715563__othello.CS;
+
+namespace _201715563__othello
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        MainWindow1 mainwindow1;
+        GameManager.eGameType m_GameType = GameManager.eGameType.PlayerVsPlayer;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        #region Events
+
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            m_GameType = GameTypePlayerRadioBtn.IsChecked == true ? GameManager.eGameType.PlayerVsPlayer : GameManager.eGameType.PlayerVsComputer;
+        }
+
+
+
+        #endregion
+
+        private void Start_Button_Click(object sender, RoutedEventArgs e)
+        {
+            mainwindow1 = new MainWindow1(8, m_GameType);
+            mainwindow1.Show();
+            this.Hide();
+        }
+
+        private void End_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).Close();
+        }
+
+        
+    }
+}
